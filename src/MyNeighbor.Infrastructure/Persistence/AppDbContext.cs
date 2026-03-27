8fe57a39d8ce7e408a5dbc9f34bfecfff5c009b5
Simpall1337 <sanduguzo1337@gmail.com>
2026-03-27 14:08:20 +0200

    added AppDbContext and BuildingConfiguration in Infrastructure

---
+++ src/MyNeighbor.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNeighbor.Application.Common.Interfaces;
+using MyNeighbor.Domain.Housing;
+
+namespace MyNeighbor.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext, IApplicationDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Building> Buildings => Set<Building>();
+        public DbSet<Apartment> Apartments => Set<Apartment>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
---
+++ src/MyNeighbor.Infrastructure/Persistence/Configurations/BuildingConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyNeighbor.Domain.Housing;
+
+namespace MyNeighbor.Infrastructure.Persistence.Configurations
+{
+    public class BuildingConfiguration : IEntityTypeConfiguration<Building>
+    {
+        public void Configure(EntityTypeBuilder<Building> builder)
+        {
+            builder.ToTable("Buildings");
+
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.OwnsOne(b => b.Address, address =>
+            {
+                address.Property(a => a.City).HasColumnName("City").IsRequired();
+                address.Property(a => a.Street).HasColumnName("Street").IsRequired();
+                address.Property(a => a.HouseNumber).HasColumnName("HouseNumber").IsRequired();
+            });
+
+            builder.HasMany(b => b.Apartments)
+                   .WithOne()
+                   .HasForeignKey(a => a.BuildingId);
+
+            builder.Navigation(b => b.Apartments)
+                   .HasField("_apartments");
+        }
+    }
+}
