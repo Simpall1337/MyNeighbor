6e8d4c67a5e2c3253e34eb9d95ecd249b025c587
Simpall1337 <sanduguzo1337@gmail.com>
2026-03-27 14:48:20 +0200

    Added DI and new Controller

---
+++ src/MyNeighbor.API/Controllers/BuildingsController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyNeighbor.Application.Housing.Commands.CreateBuilding;
+using MyNeighbor.Application.Housing.Queries.GetBuildings;
+
+namespace MyNeighbor.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BuildingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new GetBuildingsQuery(), cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateBuildingCommand command, CancellationToken cancellationToken)
+        {
+            var id = await _mediator.Send(command, cancellationToken);
+            return CreatedAtAction(nameof(GetAll), new { id }, id); 
+        }
+    }
+}
